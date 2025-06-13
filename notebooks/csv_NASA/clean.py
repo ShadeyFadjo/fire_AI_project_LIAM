5561787a78baeca5ec7f371479c02255670776d8
ShadeyFadjo <114293328+ShadeyFadjo@users.noreply.github.com>
2025-06-13 11:57:34 +0300

    update clean.py and 01_exploration.ipynb

--- notebooks/csv_NASA/clean.py
+++ notebooks/csv_NASA/clean.py
@@ -1,15 +1,13 @@
 import json
 
-# Charger le notebook
-with open("01_exploration.ipynb", "r", encoding='latin1') as f:
+file_name="01_exploration.ipynb"
+with open(file_name, "r", encoding='latin1') as f:
     data = json.load(f)
 
-# Supprimer les widgets
 if "widgets" in data.get("metadata", {}):
     del data["metadata"]["widgets"]
 
-# Sauvegarder le fichier nettoyé
-with open("01_exploration.ipynb", "w") as f:
+with open(file_name, "w") as f:
     json.dump(data, f, indent=4)
 
-print("Nettoyage terminé 🚀")
+print("Done 🚀")
