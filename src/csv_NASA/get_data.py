a5b7792c455b5d2d266df24a4082ce2eb84f113d
ShadeyFadjo <114293328+ShadeyFadjo@users.noreply.github.com>
2025-06-23 22:31:43 +0300

    update get_data, csv_NASA

--- src/csv_NASA/get_data.py
+++ src/csv_NASA/get_data.py
@@ -1,5 +1,16 @@
 import utils
 import polars as pl
 
-def get_data():
-    return utils.get_config
+config=utils.get_config()
+
+def get_data(start_date=config['firms_NASA']['data_last_date']):
+    #update config
+    #alana le MDG
+    start_date=utils.add_days(start_date,1)
+    end_date=utils.add_days(start_date)
+    file_name='modis_'+start_date+'_'+end_date
+    url=config['firms_NASA']['API_url']+start_date
+    df=pl.read_csv(url)
+    new_file=config['firms_NASA']['raw_data_location']+file_name+'.csv'
+    df.write_csv(new_file)
+    print('file: '+ new_file + ' created')
--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
 from csv_NASA import get_data
 
 if __name__ == "__main__":
-    print(get_data.get_data())
+    get_data.get_data()
