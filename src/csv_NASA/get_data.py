00e3d9093ada7627efdc778e73cb96c63ea2a7f8
ShadeyFadjo <114293328+ShadeyFadjo@users.noreply.github.com>
2025-06-24 09:35:14 +0300

    update get_data csv

--- src/csv_NASA/get_data.py
+++ src/csv_NASA/get_data.py
@@ -3,14 +3,27 @@ import polars as pl
 
 config=utils.get_config()
 
-def get_data(start_date=config['firms_NASA']['data_last_date']):
-    #update config
-    #alana le MDG
-    start_date=utils.add_days(start_date,1)
+#task
+def update_yaml_data_last_date():
+    start_date=config['firms_NASA']['data_last_date']
     end_date=utils.add_days(start_date)
+    utils.update_yaml('firms_NASA','data_last_date',end_date)
+
+# task
+def pre_save():
+    start_date=config['firms_NASA']['data_last_date']
+    end_date=utils.add_days(start_date)
+    start_date=utils.add_days(start_date,1)
     file_name='modis_'+start_date+'_'+end_date
     url=config['firms_NASA']['API_url']+start_date
     df=pl.read_csv(url)
+    df=df.drop(['country_id'])
     new_file=config['firms_NASA']['raw_data_location']+file_name+'.csv'
-    df.write_csv(new_file)
+    update_yaml_data_last_date()
+    return df,new_file
+
+# flow
+def get_data():
+    df, new_file=pre_save()
+    df.write_csv(new_file, separator=';')
     print('file: '+ new_file + ' created')
--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,20 @@
 from datetime import datetime, timedelta
 import yaml
 
+config_file='config.yaml'
+
 def add_days(date_str,dayadd=10):
     date_obj = datetime.strptime(date_str, "%Y-%m-%d")
     new_date = date_obj + timedelta(days=dayadd)
     return new_date.strftime("%Y-%m-%d")
 
-def get_config(config_file='config.yaml'):
+def get_config():
     with open(config_file, 'r') as f:
         config = yaml.safe_load(f) 
     return config
+
+def update_yaml(grp_str,params_str,new_str):
+    config=get_config()
+    config[grp_str][params_str] = new_str
+    with open(config_file, "w") as file:
+        yaml.safe_dump(config, file, default_flow_style=False,sort_keys=False)
