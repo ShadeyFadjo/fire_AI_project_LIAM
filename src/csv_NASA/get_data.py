d0ccc35f1a12b9133e052321ee8e820620cb2e84
ShadeyFadjo <114293328+ShadeyFadjo@users.noreply.github.com>
2025-06-24 11:19:59 +0300

    update get_data csv

--- src/csv_NASA/get_data.py
+++ src/csv_NASA/get_data.py
@@ -4,16 +4,19 @@ from prefect import flow, task
 
 config=utils.get_config()
 
-@task
-def update_yaml_data_last_date():
+def start_end_date():
     start_date=config['firms_NASA']['data_last_date']
     end_date=utils.add_days(start_date)
+    return start_date,end_date
+
+@task
+def update_yaml_data_last_date():
+    start_date,end_date=start_end_date()
     utils.update_yaml('firms_NASA','data_last_date',end_date)
 
 @task
 def pre_save():
-    start_date=config['firms_NASA']['data_last_date']
-    end_date=utils.add_days(start_date)
+    start_date,end_date=start_end_date()
     start_date=utils.add_days(start_date,1)
     file_name='modis_'+start_date+'_'+end_date
     url=config['firms_NASA']['API_url']+start_date
