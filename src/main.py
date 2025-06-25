ebbc20e28ba0d9a8def155872d4efa8d872db180
ShadeyFadjo <114293328+ShadeyFadjo@users.noreply.github.com>
2025-06-25 21:25:29 +0300

    pipeline csv

--- src/main.py
+++ src/main.py
@@ -1,11 +1,15 @@
-from csv_NASA import get_data as get_data_csv, preprocessing as preprocessing_csv
+from csv_NASA import get_data as get_data_csv, preprocessing as preprocessing_csv, pipeline as csv_pipeline
 from image_GEE import get_data as get_data_image
 
 if __name__ == "__main__":
     # csv_NASA
+
     # raw_file = get_data_csv.get_data()
-    raw_file='../data/raw/csv_NASA/modis/instrument_modis_2025/modis_2025-06-05_2025-06-14.csv'
-    preprocessing_csv.preprocess(raw_file)
+
+    # raw_file='../data/raw/csv_NASA/modis/instrument_modis_2025/modis_2025-06-05_2025-06-14.csv'
+    # preprocessing_csv.preprocess(raw_file)
+
+    csv_pipeline.pipeline()
 
     # gee
     # end_date='2025-06-20'
