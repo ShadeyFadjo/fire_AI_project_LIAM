e395e0490576049dfbd854213e1e5d310d3b9789
ShadeyFadjo <114293328+ShadeyFadjo@users.noreply.github.com>
2025-06-25 12:30:24 +0300

    preprocessing.py csv

--- src/csv_NASA/preprocessing.py
+++ src/csv_NASA/preprocessing.py
@@ -4,7 +4,7 @@ from prefect import flow, task
 
 config=utils.get_config()
 
-#@task(name='filter-conf-and-type')
+@task(name='filter-conf-and-type')
 def filter_conf_type(df):
     if 'type' in df.columns:
         df = df.filter((pl.col('confidence') >= 50) & (pl.col('type') == 0))
@@ -12,25 +12,23 @@ def filter_conf_type(df):
         df = df.filter(pl.col('confidence') >= 50)
     return df
 
-#@task
+@task
 def drop_columns(df):
-    return df.drop(['scan','track','satellite','instrument','confidence','version','bright_t31','daynight','type'])
+    df = df.drop(['scan','track','satellite','instrument','confidence','version','bright_t31','daynight'])
+    return df
 
-#@task
-def cast_acq_date(df):
-    return df.with_columns(pl.col("acq_date").str.strptime(pl.Date, "%d/%m/%Y"))
+# @task
+# def cast_acq_date(df):
+#     df=df.with_columns(pl.col("acq_date").str.strptime(pl.Date, "%d/%m/%Y"))
+#     return df
 
-#@task
+@task
 def write_csv(df,raw_file):
     loc_new_file=config['firms_NASA']['interim_data_location'] + raw_file.split('/')[-1]
-    df.write_csv(loc_new_file)
+    df.write_csv(loc_new_file,separator=';')
     return loc_new_file
 
-#@flow(name='preprocess-new-data')
+@flow(name='preprocess-new-data')
 def preprocess(raw_file):
-    df=pl.read_csv(new_file, separator=';')
-    df=filter_conf_type(df)
-    df=drop_columns(df)
-    df=cast_acq_date(df)
-    new_file=write_csv(df,raw_file) 
+    new_file = write_csv((drop_columns(filter_conf_type(pl.read_csv(raw_file, separator=';')))), raw_file)
     print('file processed: '+ new_file + ' created')
--- src/main.py
+++ src/main.py
@@ -3,9 +3,9 @@ from image_GEE import get_data as get_data_image
 
 if __name__ == "__main__":
     # csv_NASA
-    # new_file = get_data_csv.get_data()
-    new_file='../data/raw/csv_NASA/modis/instrument_modis_2025/modis_2025-01-01_2025-04-15'
-    preprocessing_csv.preprocess(new_file)
+    # raw_file = get_data_csv.get_data()
+    raw_file='../data/raw/csv_NASA/modis/instrument_modis_2025/modis_2025-06-05_2025-06-14.csv'
+    preprocessing_csv.preprocess(raw_file)
 
     # gee
     # end_date='2025-06-20'
